11640b5a12f6d499d068370d8f677b9e8bdea7d5
MKuto77 <Kutateladze2022@gmail.com>
2023-07-12 17:58:27 +0400

    Final Project - Mikheil Kutateladze

---
+++ script.js
@@ -0,0 +1,8 @@
+let mobileMenu = document.querySelector(".Header_ul")
+let menu = document.querySelector(".fa-bars")
+
+
+
+menu.addEventListener("click", ()=>{
+    mobileMenu.classList.add("show_hide");
+})
